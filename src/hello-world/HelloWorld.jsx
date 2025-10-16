8ff1eb6d985711bbb91fbef5c1902e8a008f864a
fadelm2 <fadelmuhammad128@gmail.com>
2025-10-16 13:09:06 +0700

    react dasar pertama

---
+++ src/hello-world/HelloWorld.jsx
@@ -0,0 +1,23 @@
+export default function HelloWorld (){
+    const props = {
+        text : "Hello World from Spead Syntax",
+    }
+    return (
+        <div>
+            <HeaderHelloWorld{...props}/>
+        <ParagraphHelloWorld/>
+        </div>
+    )
+}
+function HeaderHelloWorld({text = "Ups, lupa kasi teks"}){
+    return (
+        <h1 className="title">{text.toUpperCase()}</h1>
+    )
+}
+
+function ParagraphHelloWorld(){
+    const text = "Selamat Belajar ReactJS dari Programmer Zaman Now";
+    return (
+        <p className="content">{text.toLowerCase()}</p>
+    )
+}
---
+++ src/hello-world/main.jsx
@@ -0,0 +1,12 @@
+import {createRoot} from "react-dom/client";
+import HelloWorld from "./HelloWorld.jsx";
+import {StrictMode} from "react";
+
+
+createRoot(document.getElementById("root"))
+    .render(
+        <StrictMode>
+                <HelloWorld/>
+
+        </StrictMode>
+    )
