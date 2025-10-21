27af70401d540b4fc604da6966a92b96fa87fa2a
fadelm2 <fadelmuhammad128@gmail.com>
2025-10-21 14:47:50 +0700

    Untuk melakukan ini, kita harus mengubah lokasi State dari Component-Component itu, ke Parent Component mereka, lalu kita kirim State nya melalui Props

--- src/task/Task.jsx
+++ src/task/Task.jsx
@@ -1,36 +1,14 @@
-import {useState} from "react";
 import {useImmer} from "use-immer";
+import TaskForm from "./TaskForm.jsx";
+import TaskList from "./TaskList.jsx";
 
-
-export default function Task(){
-    const [item, setItem] = useState("");
+export default function Task() {
     const [items, setItems] = useImmer([]);
 
-    function handleChange(e){
-        setItem(e.target.value);
-    }
-
-    function handleClick(e){
-        e.preventDefault();
-        setItems((items) => {
-            items.push(item);
-        })
-        setItem("")
-    }
-    return(
+    return (
         <div>
-            <h1>Create Task</h1>
-            <form>
-                <input value={item} onChange={handleChange} />
-                <button onClick={handleClick}>Add</button>
-            </form>
-
-            <h1>List Tasks</h1>
-            <ul>
-                {items.map((item, index) =>
-                    <li key={index}>{item}</li>
-                )}
-            </ul>
+            <TaskForm setItems={setItems}/>
+            <TaskList items={items}/>
         </div>
     )
 }
