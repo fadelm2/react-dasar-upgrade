24a8d673f0de6e1cf9101311e14deb1a28d9535f
fadelm2 <fadelmuhammad128@gmail.com>
2025-10-21 11:27:40 +0700

    event handler

--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -4,6 +4,8 @@ import {StrictMode} from "react";
 import Container from "../container/Container.jsx";
 import Todolist from "../todolist/Todolist.jsx";
 import Table from "../table/Table.jsx";
+import AlertButton from "../button/AlertButton.jsx";
+import MyButton from "../button/MyButton.jsx";
 
 
 createRoot(document.getElementById("root"))
@@ -13,6 +15,14 @@ createRoot(document.getElementById("root"))
                 <HelloWorld/>
                 <Todolist/>
                 <Table/>
+                <br/>
+                <br/>
+                <AlertButton text="Click me" message="You Click me"/>
+                <AlertButton text="Click You" message="You Click You"/>
+
+                <MyButton text="Smash me" onSmash={()=> alert("You Smash Me")}/>
+                <MyButton text="Hit me" onSmash={()=> alert("You Hit Me")}/>
+
             </Container>
 
         </StrictMode>
